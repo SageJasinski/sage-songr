34449f0be5d4b248e65be765c2af4324405ee6d2
Sage Jasinski <sagejasinski@gmail.com>
2022-11-07 22:13:40 -0800

    initial commit from Spring init

---
+++ songr/src/main/java/com/sagesongr/songr/SongrApplication.java
@@ -0,0 +1,13 @@
+package com.sagesongr.songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrApplication.class, args);
+	}
+
+}
